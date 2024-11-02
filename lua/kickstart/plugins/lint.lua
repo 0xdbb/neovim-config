6f31778e4dc8e0e1cc10f8603bc9f63fa65a8a0f
Dennis <dennisboachie9@gmail.com>
2024-11-02 19:24:40 +0000

    my config

---
+++ lua/kickstart/plugins/lint.lua
@@ -0,0 +1,52 @@
+return {
+  { -- Linting
+    'mfussenegger/nvim-lint',
+    event = { 'BufReadPre', 'BufNewFile' },
+    config = function()
+      local lint = require 'lint'
+
+      -- Define linters for specific file types
+      lint.linters_by_ft = lint.linters_by_ft or {}
+      lint.linters_by_ft['markdown'] = { 'markdownlint' } -- Use markdownlint for Markdown files
+      lint.linters_by_ft['json'] = { 'jsonlint' }         -- Use jsonlint for JSON files
+
+      -- Default linter settings (commented sections kept from previous commands)
+      -- However, note that this will enable a set of default linters,
+      -- which will cause errors unless these tools are available:
+      -- {
+      --   clojure = { "clj-kondo" },
+      --   dockerfile = { "hadolint" },
+      --   inko = { "inko" },
+      --   janet = { "janet" },
+      --   json = { "jsonlint" },
+      --   markdown = { "vale" },
+      --   rst = { "vale" },
+      --   ruby = { "ruby" },
+      --   terraform = { "tflint" },
+      --   text = { "vale" }
+      -- }
+
+      -- Optionally disable specific default linters
+      -- lint.linters_by_ft['clojure'] = nil
+      -- lint.linters_by_ft['dockerfile'] = nil
+      -- lint.linters_by_ft['inko'] = nil
+      -- lint.linters_by_ft['janet'] = nil
+      -- lint.linters_by_ft['json'] = nil
+      -- lint.linters_by_ft['markdown'] = nil
+      -- lint.linters_by_ft['rst'] = nil
+      -- lint.linters_by_ft['ruby'] = nil
+      -- lint.linters_by_ft['terraform'] = nil
+      -- lint.linters_by_ft['text'] = nil
+
+      -- Autocommand to run the linter on specific events
+      local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+        group = lint_augroup,
+        callback = function()
+          lint.try_lint()
+        end,
+      })
+    end,
+  },
+}
+
---
+++ lua/kickstart/plugins/neo-tree.lua
@@ -0,0 +1,202 @@
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  cmd = 'Neotree',
+  keys = {
+    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '<leader>ee', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+  },
+  opts = {
+    filesystem = {
+      window = {
+        mappings = {
+          ['\\'] = 'close_window',
+          ['<leader>ee'] = 'close_window',
+        },
+      },
+
+      default_component_configs = {
+        container = { enable_character_fade = true },
+        indent = {
+          indent_size = 2,
+          padding = 1,
+          with_markers = true,
+          indent_marker = '│',
+          last_indent_marker = '└',
+          highlight = 'NeoTreeIndentMarker',
+          expander_collapsed = '',
+          expander_expanded = '',
+          expander_highlight = 'NeoTreeExpander',
+        },
+        icon = {
+          folder_closed = '',
+          folder_open = '',
+          folder_empty = '󰜌',
+        },
+        git_status = {
+          symbols = {
+            added = '',
+            modified = '',
+            deleted = '✖',
+            renamed = '󰁕',
+            untracked = '',
+            ignored = '',
+            unstaged = '󰄱',
+            staged = '',
+            conflict = '',
+          },
+        },
+      },
+      filtered_items = {
+        visible = true, -- Show all hidden files by default
+        hide_dotfiles = false, -- Show dotfiles
+        never_show = { -- Add exceptions here
+          '.git', -- Hide .git directory
+          '.DS_Store', -- Hide .DS_Store (macOS-specific)
+          'node_modules', -- Hide node_modules directory
+        },
+      },
+    },
+  },
+}
+
+--
+-- --
+-- return {
+--   'nvim-neo-tree/neo-tree.nvim',
+--   branch = 'v3.x',
+--   version = '*',
+--   dependencies = {
+--     'nvim-lua/plenary.nvim',
+--     'nvim-tree/nvim-web-devicons', -- recommended for icons
+--     'MunifTanjim/nui.nvim',
+--     {
+--       's1n7ax/nvim-window-picker',
+--       version = '2.*',
+--       config = function()
+--         require('window-picker').setup {
+--           filter_rules = {
+--             include_current_win = false,
+--             autoselect_one = true,
+--             bo = {
+--               filetype = { 'neo-tree', 'neo-tree-popup', 'notify' },
+--               buftype = { 'terminal', 'quickfix' },
+--             },
+--           },
+--         }
+--       end,
+--     },
+--   },
+--   cmd = 'Neotree',
+--   keys = {
+--     { '\\',         ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+--     { '<leader>ee', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+--   },
+--   config = function()
+--     vim.fn.sign_define('DiagnosticSignError', { text = ' ', texthl = 'DiagnosticSignError' })
+--     vim.fn.sign_define('DiagnosticSignWarn', { text = ' ', texthl = 'DiagnosticSignWarn' })
+--     vim.fn.sign_define('DiagnosticSignInfo', { text = ' ', texthl = 'DiagnosticSignInfo' })
+--     vim.fn.sign_define('DiagnosticSignHint', { text = '󰌵', texthl = 'DiagnosticSignHint' })
+--
+--     require('neo-tree').setup {
+--       close_if_last_window = false,
+--       popup_border_style = 'rounded',
+--       enable_git_status = true,
+--       enable_diagnostics = true,
+--       open_files_do_not_replace_types = { 'terminal', 'trouble', 'qf' },
+--       sort_case_insensitive = false,
+--       filesystem = {
+--         window = {
+--           mappings = {
+--             ['\\'] = 'close_window',
+--             ['<leader>ee'] = 'close_window',
+--           },
+--         },
+--         filtered_items = {
+--           visible = true,
+--           hide_dotfiles = false,
+--           never_show = { '.git', '.DS_Store', 'node_modules' },
+--         },
+--         follow_current_file = {
+--           enabled = false,
+--           leave_dirs_open = false,
+--         },
+--         group_empty_dirs = false,
+--         hijack_netrw_behavior = 'open_default',
+--         use_libuv_file_watcher = false,
+--         window = {
+--           mappings = {
+--             ['<bs>'] = 'navigate_up',
+--             ['.'] = 'set_root',
+--             ['H'] = 'toggle_hidden',
+--             ['/'] = 'fuzzy_finder',
+--             ['D'] = 'fuzzy_finder_directory',
+--             ['<c-x>'] = 'clear_filter',
+--           },
+--         },
+--       },
+--       default_component_configs = {
+--         container = { enable_character_fade = true },
+--         indent = {
+--           indent_size = 2,
+--           padding = 1,
+--           with_markers = true,
+--           indent_marker = '│',
+--           last_indent_marker = '└',
+--           highlight = 'NeoTreeIndentMarker',
+--           expander_collapsed = '',
+--           expander_expanded = '',
+--           expander_highlight = 'NeoTreeExpander',
+--         },
+--         icon = {
+--           folder_closed = '',
+--           folder_open = '',
+--           folder_empty = '󰜌',
+--         },
+--         git_status = {
+--           symbols = {
+--             added = '',
+--             modified = '',
+--             deleted = '✖',
+--             renamed = '󰁕',
+--             untracked = '',
+--             ignored = '',
+--             unstaged = '󰄱',
+--             staged = '',
+--             conflict = '',
+--           },
+--         },
+--       },
+--       buffers = {
+--         follow_current_file = {
+--           enabled = true,
+--           leave_dirs_open = false,
+--         },
+--         group_empty_dirs = true,
+--       },
+--       git_status = {
+--         window = {
+--           position = 'float',
+--           mappings = {
+--             ['A'] = 'git_add_all',
+--             ['gu'] = 'git_unstage_file',
+--             ['ga'] = 'git_add_file',
+--             ['gr'] = 'git_revert_file',
+--             ['gc'] = 'git_commit',
+--             ['gp'] = 'git_push',
+--           },
+--         },
+--       },
+--     }
+--
+--     vim.cmd [[nnoremap \ :Neotree reveal<cr>]]
+--   end,
+-- }
